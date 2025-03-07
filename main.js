e0889af070a3f0ed4b78ca555e021cffc7e4b585
Peter-Kisselev <peter.kisselev@gmail.com>
2025-03-07 14:13:42 -0500

    end of class
    Co-authored-by: Taran-Bhandari <Taran-Bhandari@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 let score;
+
+let powerAddBought;
+let powerMultipliedBought;
+let autoclickers;
+
+let clickPowerRaw;
+let multiplier;
+let clickPower;
 let increment;
+//
+let allTimeScore;
+//
+let addPrice;
+let multPrice;
+let autoPrice;
+
+let addPriceScale;
+let multPriceScale;
+let autoPriceScale;
+
 
 const selectPokemon = (choice) => {
     const selectedPokemon = choice;
@@ -20,17 +39,126 @@ const selectPokemon = (choice) => {
 	finalImg.style.backgroundRepeat = "no-repeat";
 
 	document.getElementById('trick').click();
+}
+
+const addScore = () => {
+	score += clickPower //Placeholder
+	allTimeScore += clickPower;
+}
+
+const updateScore = () => {
+	document.getElementById("score").textContent = "Score: " + score;
+	document.getElementById("alltimeClicks").textContent = allTimeScore;
+	// console.log(score);
+}
+
+const updatePrice = () => {
+	const addPriceIndicator = document.getElementById("addPrice");
+	const multPriceIndicator = document.getElementById("multPrice");
+	const autoPriceIndicator = document.getElementById("autoPrice");
+
+	addPriceIndicator.textContent = addPrice;
+	multPriceIndicator.textContent = multPrice;
+	autoPriceIndicator.textContent = autoPrice;
 
 }
 
 const pokeClick = (ev) => {
+	addScore();
 	updateScore();
+	updateAllowed();
 }
 
-const updateScore = () => { 
-	score += increment //Placeholder
-	document.getElementById("score").textContent = score;
-	console.log(score);
+const updateAllowed = () => {
+	const doAdd = document.getElementById("incrementPower");
+	const doMult = document.getElementById("multiplyPower");
+	const doAuto = document.getElementById("autoclicker");
+
+	if(score >= addPrice) {
+		doAdd.classList.add("selectable");
+		doAdd.classList.remove("greyedOut");
+	} else {
+		doAdd.classList.remove("selectable");
+		doAdd.classList.add("greyedOut");
+	}
+	if(score >= multPrice) {
+		doMult.classList.add("selectable");
+		doMult.classList.remove("greyedOut");
+	} else {
+		doMult.classList.remove("selectable");
+		doMult.classList.add("greyedOut");
+	}
+	if(score >= autoPrice) {
+		doAuto.classList.add("selectable");
+		doAuto.classList.remove("greyedOut");
+	} else {
+		doAuto.classList.remove("selectable");
+		doAuto.classList.add("greyedOut");
+	}
+}
+
+const updatePower = () => {
+	clickPower = clickPowerRaw*multiplier;
+	document.getElementById("clickPower").textContent = clickPower;
+}
+
+const addPower = (ev) => {
+	if (score >= addPrice){
+		score -= addPrice;
+		updateScore();
+		addPrice += addPriceScale;
+		powerAddBought += 1;
+		document.getElementById("addInc").textContent = powerAddBought;
+		clickPowerRaw = 1+powerAddBought;
+
+		document.getElementById("clickPowerRaw").textContent = clickPowerRaw;
+		updatePower();
+		updatePrice();
+	}
+	else{
+		console.log("insufficient funds");
+	}
+}
+
+const multPower = (ev) => {
+	if (score >= multPrice){
+		score -= multPrice;
+		updateScore();
+		multPrice += multPriceScale;
+		powerMultipliedBought += 1;
+		document.getElementById("multInc").textContent = powerMultipliedBought;
+		multiplier = Math.pow(2, powerMultipliedBought);
+
+		document.getElementById("multiplierVal").textContent = multiplier;
+		updatePower();
+		updatePrice();
+	}
+	else{
+		console.log("insufficient funds");
+	}
+}
+
+const addClicker = (ev) => {
+	if (score >= autoPrice){
+		score -= autoPrice;
+		updateScore();
+		autoPrice += autoPriceScale;
+		autoclickers += 1;
+		document.getElementById("addClicker").textContent = autoclickers;
+		document.getElementById("autoclickers").textContent = autoclickers;
+
+		addAutoclicker();
+
+		updatePower();
+		updatePrice();
+	}
+	else{
+		console.log("insufficient funds");
+	}
+}
+
+const addAutoclicker = () => {
+
 }
 
 let recieved = function () { };
@@ -44,8 +172,26 @@ function waitForChange() {
 
 document.body.onload = async () => {
 	await waitForChange();
-	score = 0;
-	increment = 1;
-	console.log("chosen");
+	console.log("Game ready");
 
+	score = 0;
+	allTimeScore = 0;
+	clickPowerRaw = 1;
+	multiplier = 1;
+	clickPower = 1;
+	increment = 1;
+
+	powerAddBought = 0;
+	powerMultipliedBought = 0;
+	autoclickers = 0;
+
+	addPrice = 5;
+	multPrice = 1000;
+	autoPrice = 10000;
+
+	addPriceScale = 10;
+	multPriceScale = 1000;
+	autoPriceScale = 10000;
+
+	updatePrice();
 }
